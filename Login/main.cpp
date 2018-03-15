a4b827b863e725cc26e3ba1142047ea3c2e8a714
shooyin <j_le_75@hotmail.com>
2018-03-14 17:01:45 -0700

    Update main.cpp

--- Login/main.cpp
+++ Login/main.cpp
@@ -13,7 +13,6 @@ void showForgotPasswordScreen(hashi hashy);
 void showAdminScreen(hashi hashy);
 
 int main() {
-
 	hashi hashy;
 
 	showOpeningScreen(hashy);
@@ -26,50 +25,47 @@ void showOpeningScreen(hashi hashy) {
 	char choice = ' ';
 	bool lock = true;
 
-	cout << "Apple Bottom Jeans\n\n";
-	cout << "(1) Login\n";
-	cout << "(2) Create Account\n";
-	cout << "(3) Forgot Password\n";
-	cout << "(4) Exit\n\n";
-	cout << "Choice: ";
+	cout << "\t\t   ****** Welcome To Apple Bottom Jeans ****** \n";
+	cout << "\n\n\t\t   Please Select From The Following Menu ";
+	cout << "\n\n\t\t\t(1) Login\n";
+	cout << "\n\n\t\t\t(2) Create Account\n";
+	cout << "\n\n\t\t\t(3) Forgot Password\n";
+	cout << "\n\n\t\t\t(4) Exit\n\n";
+	cout << "\n\n\t\t\tChoice: ";
 	cin >> choice;
 
 	switch (choice) {
 	case '1': system("cls");
-			  showLoginScreen(hashy);
-			  break;
+		showLoginScreen(hashy);
+		break;
 	case '2': system("cls");
-			  showCreateAccountScreen(hashy);
-			  break;
+		showCreateAccountScreen(hashy);
+		break;
 	case '3': system("cls");
-			  showForgotPasswordScreen(hashy);
-			  break;
+		showForgotPasswordScreen(hashy);
+		break;
 	case '4': cout << "\nGoodbye!\n" << endl;
-			  system("exit");
-			  break;
+		system("exit");
+		break;
 	case '5': system("cls");
-			  showAdminScreen(hashy);
-			  break;
+		showAdminScreen(hashy);
+		break;
 	default:  system("cls");
-			  showOpeningScreen(hashy);
+		showOpeningScreen(hashy);
 	}
 }
 
 void showLoginScreen(hashi hashy) {
 	string username, password;
 
-	cout << "Username: ";
+	cout << "\t\t   ****** Login ****** \n";
+	cout << "\n\n\t\tUsername: ";
 	cin >> username;
-	cout << "Password: ";
+	cout << "\n\n\t\tPassword: ";
 	cin >> password;
 
 	if (hashy.confirmUser(username, password) == true) {
-		/*
-		This is where you put your code in Tran,
-		replace this area and the comment with the start
-		of the Database menu
-		  */
-		cout << "Passed" << endl;
+		cout << "\n\n\t\tPassed" << endl;
 	}
 
 	system("pause");
@@ -80,18 +76,19 @@ void showLoginScreen(hashi hashy) {
 void showCreateAccountScreen(hashi hashy) {
 	string username, password;
 
-	cout << "Username: ";
+	cout << "\t\t   ****** Create Account ****** \n";
+	cout << "\n\n\t\t\tUsername: ";
 	cin >> username;
-	cout << "Password: ";
+	cout << "\n\n\t\t\tPassword: ";
 	cin >> password;
 
 	if (hashy.hasDuplicate(username)) {
-		cout << "\n\n" << username << " already exists" << endl;
+		cout << "\n\n\t\t\t" << username << " already exists" << endl;
 	}
 	else {
 		hashy.addUser(username, password);
 		hashy.addToTextFile(username, password);
-		cout << "\n\n" << username << " was successfully created" << endl;
+		cout << "\n\n\t\t\t" << username << " was successfully created" << endl;
 	}
 	system("pause");
 	system("cls");
@@ -101,14 +98,17 @@ void showCreateAccountScreen(hashi hashy) {
 void showForgotPasswordScreen(hashi hashy) {
 	string username;
 
-	cout << "Forgot Your Password?\n\n";
+	cout << "\t\tForgot Your Password?\n\n";
 
 	while (username != "exit") {
-		cout << "Search: ";
+		cout << "\n\t\tSearch: ";
 		cin >> username;
 
-		if (username != "exit")
+		if (username != "exit") {
+			cout << "\t\t";
 			hashy.findPassword(username);
+		}
+		cout << endl;
 	}
 	system("pause");
 	system("cls");
@@ -124,7 +124,7 @@ void showAdminScreen(hashi hashy) {
 	bool lock = true;
 	int choice = 0;
 	string username, password;
-
+	cout << "\t\t   ****** DEBUG ****** \n";
 	cout << "1. Print Table\n";
 	cout << "2. Remove User\n";
 	cout << "3. Return to Main Menu\n";
